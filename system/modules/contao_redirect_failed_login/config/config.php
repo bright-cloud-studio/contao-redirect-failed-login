530e455fa3f9f2d4658311df893e59e10ae5d718
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-22 10:40:15 -0500

    Update config.php

--- system/modules/contao_redirect_failed_login/config/config.php
+++ system/modules/contao_redirect_failed_login/config/config.php
@@ -10,4 +10,4 @@
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
 
-$GLOBALS['FE_MOD']['user']['login'] = 'Bcs\Module\ModLoginRedirect';
+$GLOBALS['FE_MOD']['user']['login'] = 'Bcs\Module\ModuleLoginRedirect';
