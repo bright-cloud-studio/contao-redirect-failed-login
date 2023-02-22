86490489c10a7b015ed35a9d557c00a50bcb2c37
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-22 09:15:31 -0500

    Create tl_module.php

---
+++ system/modules/contao_redirect_failed_login/languages/en/tl_module.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Redirect Failed Login
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-redirect-failed-login
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_module']['jumpToFailed']          = array('Redirect (Login Failed) page', 'Please choose the page to which visitors will be redirected when failing a login attempt.');
