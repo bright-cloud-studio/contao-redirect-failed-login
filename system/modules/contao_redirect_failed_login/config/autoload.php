1212f49f4dd7a872e5ce065904ed64947c7c132f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-22 10:29:31 -0500

    Create autoload.php

---
+++ system/modules/contao_redirect_failed_login/config/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Redirect Failed Login
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-redirect-failed-login
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Register the classes */
+ClassLoader::addClasses(array
+(
+    'Bcs\Module\ModLoginRedirect'               => 'system/modules/contao_redirect_failed_login/library/Bcs/Module/ModLoginRedirect.php'
+));
