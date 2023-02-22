0f69954cde114f18b0feddcdf261de0f2418a687
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-22 09:56:29 -0500

    Create config.php

---
+++ system/modules/contao_redirect_failed_login/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Redirect Failed Login
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-redirect-failed-login
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+use Bcs\ModuleLogin;
+
+$GLOBALS['FE_MOD']['user']['login'] = ModuleLogin::class;
